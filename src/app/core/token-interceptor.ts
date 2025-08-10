762cbe412faa5321b1c51ef4dbd21540607ccf81
YulianaChambi <chambiy447@gmail.com>
2025-08-10 18:22:14 -0500

    MantCliente + Service

--- src/app/core/token-interceptor.ts
+++ src/app/core/token-interceptor.ts
@@ -1,5 +1,6 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
   const t = localStorage.getItem('token');
+  console.log('Token enviado en', req.url, ':', t)
   return next(t ? req.clone({ setHeaders: { Authorization: `Bearer ${t}` } }) : req);
 };
